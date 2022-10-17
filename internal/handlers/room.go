d7a412099c5fc718ccce3bacbbf419a4a9ae5ca7
roxyash <roxyash@mail.ru>
2022-10-17 15:42:53 +0600

    refactor room func and implement roomviewerwebsocket func

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"fmt"
-	w "github.com/roxyash/pkg/webrtc"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 	gguid "github.com/google/uuid"
+	w "github.com/roxyash/pkg/webrtc"
 )
 
 func RoomCreate(c *fiber.Ctx) error {
@@ -20,7 +22,21 @@ func Room(c *fiber.Ctx) error {
 		return nil
 	}
 
+	ws := "ws"
+	if os.Getenv("ENVIRONMENT") == "PRODUCTION"{
+		ws := "wss"
+	}
+
+
 	uuid, suuid, _ := createOrGetRoom(uuid)
+	return c.Render("peer", fiber.Map{
+		"RoomWebSocketAddr": fmt.Sprintf("%s://%s/room%s/websocket/", ws, c.Hostname(), uuid),
+		"RoomLink": fmt.Sprintf("%s://%s/room/%s", c.Protocol(), c.Hostname(), uuid),
+		"ChatWebSocketAddr":fmt.Sprintf("%s://%s/room/%s/chat/websocket", ws, c.Hostname(), uuid),
+		"ViewerWebSocketAddr":fmt.Sprintf("%s://%s/room/%s/viewer/websocket", ws, c.Hostname(), uuid),
+		"StreamLink": fmt.Sprintf("%s://%s/stream/%s", c.Protocol(), c.Hostname(), suuid),
+		"Type": "room",
+	}, "layouts/main")
 }
 
 func RoomWebsocket(c *websocket.Conn) {
@@ -39,7 +55,19 @@ func createOrGetRoom(uuid string) (string, string, Room){
 }
 
 func RoomViewerWebsocket(c *websocket.Conn) {
+	uuid := c.Params("uuid")
+	if uuid  == "" {
+		return
+	}
 
+	w.RoomsLock.Lock()
+	if peer, ok := w.Rooms[uuid]; ok {
+		w.RoomsLock.Unlock()
+		roomViewerConn(c, peer.Peers)
+		return 
+	}
+
+	w.RoomsLock.Unlock()
 }
 
 func roomViewerConn(c *websocket.Conn, p *w.Peers) {
