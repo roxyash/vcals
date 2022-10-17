258d1caa40331e7c6870296ddd68ddce3973848c
roxyash <roxyash@mail.ru>
2022-10-17 15:11:20 +0600

    implemet readpump method and peerachatconn func

--- pkg/chat/client.go
+++ pkg/chat/client.go
@@ -1,16 +1,23 @@
 package chat
 
 import (
+	"bytes"
+	"log"
 	"time"
 
 	"github.com/fasthttp/websocket"
 )
 
-const(
-	writeWait = 10 * time.Second
-	pongWait = 60 * time.Second
-	pingPeriod = (pongWait * 9) / 10
+const (
+	writeWait      = 10 * time.Second
+	pongWait       = 60 * time.Second
+	pingPeriod     = (pongWait * 9) / 10
 	maxMessageSize = 512
+)
+
+var (
+	newLine = []byte{'\n'}
+	space   = []byte{' '}
 )
 
 type Client struct {
@@ -19,15 +26,51 @@ type Client struct {
 	Send chan []byte
 }
 
+var upgrader = websocket.FastHTTPUpgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func (c *Client) readPump() {
+	defer func() {
+		c.Hub.ungregister <- c
+		c.Conn.Close()
+	}()
 
+	c.Conn.SetReadLimit(maxMessageSize)
+	c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.Conn.SetPongHandler(func(string) error {
+		c.Conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
+	for {
+		_, message, err := c.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+
+		message = bytes.TrimSpace(bytes.Replace(message, newLine, space, -1))
+		c.Hub.broadcast <- message 
+	}
 }
 
 func (c *Client) writePump() {
-	
+
 }
 
-func PeerChatConn() {
-	
+func PeerChatConn(c *websocket.Conn, hub *Hub) {
+	client := &Client{
+		Hub:  hub,
+		Conn: c,
+		Send: make(chan []byte, 256),
+	}
+
+	client.Hub.register <- client
+
+	go client.writePump()
+	client.readPump()
 }
