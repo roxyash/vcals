cf13dc755ccc161a7c5fe4abf033e1eedb2ea745
s.karataev <s.karataev@atlas.online>
2022-10-20 01:35:49 +0600

    create stream file, create func stream and implement this, and 3 func without implementation

--- internal/handlers/stream.go
+++ internal/handlers/stream.go
@@ -1,1 +1,52 @@
 package handlers
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	w "github.com/roxyash/vcals/pkg/webrtc"
+)
+
+func Stream(c *fiber.Ctx) error {
+	suuid := c.Params("suuid")
+	if suuid == "" {
+		c.Status(400)
+		return nil
+	}
+
+	ws := "ws"
+	if os.Getenv("ENVIRONMENT") == "PRODUCTION" {
+		ws = "wss"
+	}
+
+	w.RoomsLock.Lock()
+	if _, ok := w.Streams[suuid]; ok {
+		w.RoomsLock.Unlock()
+		return c.Render("stream", fiber.Map{
+			"StreamWebSocketAddr": fmt.Sprintf("%s://%s/stream/%s/websocket", ws, c.Hostname(), suuid),
+			"ChatWebsocketAddr":   fmt.Sprintf("%s://%s/stream/%s/chat/websocket", ws, c.Hostname(), suuid),
+			"ViewerWebsocketAddr": fmt.Sprintf("%s://%s/stream/%s/viewer/websocket", ws, c.Hostname(), suuid),
+			"Type":                "stream",
+		}, "layouts/main")
+	}
+
+	w.RoomsLock.Unlock()
+	return c.Render("stream", fiber.Map{
+		"NoStream:" : "true",
+		"Leave": "true",
+	}, "layouts/main")
+}
+
+func StreamWebSocket(c *websocket.Conn) {
+
+}
+
+func StreamViewerWebSocket(c *websocket.Conn) {
+
+}
+
+func viewerConn(c websocket.Conn, p *w.Peers) {
+
+}
