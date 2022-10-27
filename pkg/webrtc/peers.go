7b7bb819886f7e9addedbf320256ec0a12632021
roxyash <roxyash@mail.ru>
2022-10-27 23:25:17 +0600

    implement AddTrack function

--- pkg/webrtc/peers.go
+++ pkg/webrtc/peers.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gofiber/websocket/v2"
 	"github.com/roxyash/vcals/pkg/chat"
+	"github.com/sirupsen/logrus"
 )
 
 type Room struct {
@@ -36,7 +37,19 @@ func (t *ThreadSafeWriter) WriteJSON(v interface{}) error {
 }
 
 func (p *Peers) AddTrack(t *webrtc.TrackRemote) *webrtc.TrackLocalStaticRTP {
+	p.ListLock.Lock()
+	defer func() {
+		p.ListLock.Unlock()
+		p.SignalPeerConnection()
+	}()
 
+	trackLocal, err := webrtc.NewTrackLocalStaticRTP(t.Codec().RTPCodecCapability, t.ID(), t.StreamID())
+	if err != nil {
+		logrus.Infof(err.Error())
+	}
+
+	p.TrackLocals[t.ID()] = trackLocal
+	return trackLocal
 }
 
 func (p *Peers) RemoveTrack(t *webrtc.TrackLocalStaticRTP) {
